aef7921cfd4282ac7ab9f875a3fd8780ef535987
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-05-08 19:47:52 +0530

    2nd commit

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
-const express = require("express");
-const { join } = require("path");
-const morgan = require("morgan");
+const express = require('express');
+const { join, resolve } = require('path');
+const morgan = require('morgan');
 const app = express();
 
 const port = process.env.SERVER_PORT || 3000;
 
-app.use(morgan("dev"));
-app.use(express.static(join(__dirname, "build")));
+app.use(morgan('dev'));
+app.use(express.static(join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(resolve(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
