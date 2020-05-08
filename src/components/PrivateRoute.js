2bb64a674baffff8f0383863ea73ec4ca9b0e35b
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-05-09 01:43:38 +0530

    test 3

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
-import { Route, withRouter } from "react-router-dom";
-import { useAuth0 } from "../react-auth0-spa";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Route, withRouter } from 'react-router-dom';
+import { useAuth0 } from '../react-auth0-spa';
 
 const PrivateRoute = ({ component: Component, path, ...rest }) => {
   const { loading, isAuthenticated, loginWithRedirect } = useAuth0();
@@ -12,13 +12,13 @@ const PrivateRoute = ({ component: Component, path, ...rest }) => {
     }
     const fn = async () => {
       await loginWithRedirect({
-        appState: { targetUrl: path }
+        appState: { targetUrl: window.location.pathname },
       });
     };
     fn();
   }, [loading, isAuthenticated, loginWithRedirect, path]);
 
-  const render = props =>
+  const render = (props) =>
     isAuthenticated === true ? <Component {...props} /> : null;
 
   return <Route path={path} render={render} {...rest} />;
@@ -29,8 +29,8 @@ PrivateRoute.propTypes = {
     .isRequired,
   path: PropTypes.oneOfType([
     PropTypes.string,
-    PropTypes.arrayOf(PropTypes.string)
-  ]).isRequired
+    PropTypes.arrayOf(PropTypes.string),
+  ]).isRequired,
 };
 
 export default withRouter(PrivateRoute);
