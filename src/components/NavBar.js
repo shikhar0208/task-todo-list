8ab57ef8cff0df22b09c8d490a36f56a671a5a62
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-05-09 12:15:13 +0530

    set diff route for todo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { NavLink as RouterNavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from 'react';
+import { NavLink as RouterNavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import {
   Collapse,
@@ -15,10 +15,10 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
-} from "reactstrap";
+  DropdownItem,
+} from 'reactstrap';
 
-import { useAuth0 } from "../react-auth0-spa";
+import { useAuth0 } from '../react-auth0-spa';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,35 +27,48 @@ const NavBar = () => {
 
   const logoutWithRedirect = () =>
     logout({
-      returnTo: window.location.origin
+      returnTo: window.location.origin,
     });
 
   return (
-    <div className="nav-container">
-      <Navbar color="light" light expand="md">
+    <div className='nav-container'>
+      <Navbar color='light' light expand='md'>
         <Container>
-          <NavbarBrand className="logo" />
+          <NavbarBrand className='logo' />
           <NavbarToggler onClick={toggle} />
           <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink
-                  tag={RouterNavLink}
-                  to="/"
-                  exact
-                  activeClassName="router-link-exact-active"
-                >
-                  Home
-                </NavLink>
-              </NavItem>
+            <Nav className='mr-auto' navbar>
+              {isAuthenticated ? (
+                <NavItem>
+                  <NavLink
+                    tag={RouterNavLink}
+                    to='/mytodos'
+                    exact
+                    activeClassName='router-link-exact-active'
+                  >
+                    Todos
+                  </NavLink>
+                </NavItem>
+              ) : (
+                <NavItem>
+                  <NavLink
+                    tag={RouterNavLink}
+                    to='/'
+                    exact
+                    activeClassName='router-link-exact-active'
+                  >
+                    Home
+                  </NavLink>
+                </NavItem>
+              )}
             </Nav>
-            <Nav className="d-none d-md-block" navbar>
+            <Nav className='d-none d-md-block' navbar>
               {!isAuthenticated && (
                 <NavItem>
                   <Button
-                    id="qsLoginBtn"
-                    color="primary"
-                    className="btn-margin"
+                    id='qsLoginBtn'
+                    color='primary'
+                    className='btn-margin'
                     onClick={() => loginWithRedirect({})}
                   >
                     Log in
@@ -64,29 +77,29 @@ const NavBar = () => {
               )}
               {isAuthenticated && (
                 <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret id="profileDropDown">
+                  <DropdownToggle nav caret id='profileDropDown'>
                     <img
                       src={user.picture}
-                      alt="Profile"
-                      className="nav-user-profile rounded-circle"
-                      width="50"
+                      alt='Profile'
+                      className='nav-user-profile rounded-circle'
+                      width='50'
                     />
                   </DropdownToggle>
                   <DropdownMenu>
                     <DropdownItem header>{user.name}</DropdownItem>
                     <DropdownItem
                       tag={RouterNavLink}
-                      to="/profile"
-                      className="dropdown-profile"
-                      activeClassName="router-link-exact-active"
+                      to='/profile'
+                      className='dropdown-profile'
+                      activeClassName='router-link-exact-active'
                     >
-                      <FontAwesomeIcon icon="user" className="mr-3" /> Profile
+                      <FontAwesomeIcon icon='user' className='mr-3' /> Profile
                     </DropdownItem>
                     <DropdownItem
-                      id="qsLogoutBtn"
+                      id='qsLogoutBtn'
                       onClick={() => logoutWithRedirect()}
                     >
-                      <FontAwesomeIcon icon="power-off" className="mr-3" /> Log
+                      <FontAwesomeIcon icon='power-off' className='mr-3' /> Log
                       out
                     </DropdownItem>
                   </DropdownMenu>
@@ -94,11 +107,11 @@ const NavBar = () => {
               )}
             </Nav>
             {!isAuthenticated && (
-              <Nav className="d-md-none" navbar>
+              <Nav className='d-md-none' navbar>
                 <NavItem>
                   <Button
-                    id="qsLoginBtn"
-                    color="primary"
+                    id='qsLoginBtn'
+                    color='primary'
                     block
                     onClick={() => loginWithRedirect({})}
                   >
@@ -109,35 +122,35 @@ const NavBar = () => {
             )}
             {isAuthenticated && (
               <Nav
-                className="d-md-none justify-content-between"
+                className='d-md-none justify-content-between'
                 navbar
                 style={{ minHeight: 170 }}
               >
                 <NavItem>
-                  <span className="user-info">
+                  <span className='user-info'>
                     <img
                       src={user.picture}
-                      alt="Profile"
-                      className="nav-user-profile d-inline-block rounded-circle mr-3"
-                      width="50"
+                      alt='Profile'
+                      className='nav-user-profile d-inline-block rounded-circle mr-3'
+                      width='50'
                     />
-                    <h6 className="d-inline-block">{user.name}</h6>
+                    <h6 className='d-inline-block'>{user.name}</h6>
                   </span>
                 </NavItem>
                 <NavItem>
-                  <FontAwesomeIcon icon="user" className="mr-3" />
+                  <FontAwesomeIcon icon='user' className='mr-3' />
                   <RouterNavLink
-                    to="/profile"
-                    activeClassName="router-link-exact-active"
+                    to='/profile'
+                    activeClassName='router-link-exact-active'
                   >
                     Profile
                   </RouterNavLink>
                 </NavItem>
                 <NavItem>
-                  <FontAwesomeIcon icon="power-off" className="mr-3" />
+                  <FontAwesomeIcon icon='power-off' className='mr-3' />
                   <RouterNavLink
-                    to="#"
-                    id="qsLogoutBtn"
+                    to='#'
+                    id='qsLogoutBtn'
                     onClick={() => logoutWithRedirect()}
                   >
                     Log out
