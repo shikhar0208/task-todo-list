4e76bc3907e8c49e4df8e955307b4764050a937a
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-05-09 21:37:52 +0530

    final commit

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,13 @@
 import React, { Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
 import { useAuth0 } from '../react-auth0-spa';
 import Hero from '../components/Hero';
 
 const Home = () => {
+  const { isAuthenticated } = useAuth0();
+  if (isAuthenticated) {
+    return <Redirect to='/mytodos' />;
+  }
   return (
     <Fragment>
       <Hero />
