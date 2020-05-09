e8373008691eba28b88c567fda6a8027c6530487
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-05-09 21:32:43 +0530

    remove redirect from home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,8 @@
 import React, { Fragment } from 'react';
-import { Redirect } from 'react-router-dom';
 import { useAuth0 } from '../react-auth0-spa';
 import Hero from '../components/Hero';
 
 const Home = () => {
-  const { isAuthenticated } = useAuth0();
-  if (isAuthenticated) {
-    return <Redirect to='/mytodos' />;
-  }
   return (
     <Fragment>
       <Hero />
